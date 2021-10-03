9b2f52be473243bebf98f06d77e847cca80a5d9d
AnatolijusZ <84323444+AnatolijusZ@users.noreply.github.com>
2021-10-03 21:55:43 +0300

    character creation menu

--- src/Components/App.jsx
+++ src/Components/App.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter as Router, Switch, Route, useParams } from 'react-router-dom';
+import { BrowserRouter as Router, Switch, Route, Link} from 'react-router-dom';
 import CharCreation from "./CreateChar";
 
 
@@ -10,12 +10,20 @@ function App() {
      
         return (
             <Router>
-            <div className = "main-menu">
-                <Route path={"/:create"}>
-                <button onClick={() => <CharCreation></CharCreation>}>Create character</button>
-                </Route>
-                <button>Load existing character (not implemented)</button>
-            </div>
+                <Switch>            
+                    <Route exact path={"/"}>
+                    <div className = "main-menu">
+                        <Link to="/create"> <button>Create character</button></Link>
+                        <button>Load existing character (not implemented)</button>
+                    </div>
+                    </Route>
+
+                    <Route path={"/create"}>
+                        <div className="char-creation-menu">
+                        <CharCreation></CharCreation>
+                        </div>
+                    </Route>
+                </Switch>
             </Router>
         )
     
--- src/Components/CreateChar.jsx
+++ src/Components/CreateChar.jsx
@@ -1,15 +1,24 @@
 import React, {useState} from "react";
 import getId from '../Shared/getId';
+import { useRouteMatch } from "react-router";
 
 function CharCreation() {
      const [character, setCharacter] = useState([]);
      const [nameInput, setNameInput] = useState("");
+     const [str, setStr] = useState("5");
+     const [vit, setVit] = useState("5");
+     const [dex, setDex] = useState("5");
+
+     //let { path, url } = useRouteMatch();
 
 
     const newPlayer = () => {
         const player = {
             id: getId(),
             name: nameInput,
+            strength: str,
+            vitality: vit,
+            dexterity: dex
         }
 
     }
@@ -17,8 +26,18 @@ function CharCreation() {
         setNameInput(e.target.value)
         };
 
-    return (
-        <input type="text" value={nameInput} onChange={NameInputHandler}></input>
+        return (<>
+            <div className="stats">
+            Strength: {str}
+            <button>Increase</button>
+            Vitality: {vit}
+            <button>Increase</button>
+            Dexterity: {dex}
+            <button>Increase</button>
+            Enter your character name:
+            <input type="text" value={nameInput} onChange={NameInputHandler}/>
+        </div>
+        </>
     )
 }
 export default CharCreation;
