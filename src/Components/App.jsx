0dfb2b415b2f7927f8bae3f0268e7cf57beb273a
AnatolijusZ <84323444+AnatolijusZ@users.noreply.github.com>
2021-10-04 20:43:50 +0300

    changes

--- src/Components/App.jsx
+++ src/Components/App.jsx
@@ -1,11 +1,32 @@
-import React from "react";
+import React, {useState} from "react";
 import { BrowserRouter as Router, Switch, Route, Link} from 'react-router-dom';
 import CharCreation from "./CreateChar";
+import getId from "../Shared/getId";
 
 
 function App() {
     
+    const [characters, setCharacters] = useState([]);
+    const [nameInput, setNameInput] = useState("");
+    const [str, setStr] = useState(5);
+    const [vit, setVit] = useState(5);
+    const [dex, setDex] = useState(5);
+
     
+
+    const newCharacter = () => {
+       const player = {
+           id: getId(),
+           name: nameInput,
+           strength: str,
+           vitality: vit,
+           dexterity: dex
+       }
+       const charactersCopy = characters.slice();
+       charactersCopy.push(player);
+       setCharacters (charactersCopy);
+       localStorage.setItem('allCharacters', JSON.stringify(charactersCopy));
+    }
 
      
         return (
@@ -13,7 +34,9 @@ function App() {
                 <Switch>            
                     <Route exact path={"/"}>
                     <div className = "main-menu">
-                        <Link to="/create"> <button>Create character</button></Link>
+                        <Link to="/create"> 
+                        <button  onClick={() => newCharacter(), characters.map(b => <CharCreation key={b.id} name={b.name} str={b.strength} vit={b.vitality} dex={b.dexterity}/> ) }>Create character</button>
+                        </Link>
                         <button>Load existing character (not implemented)</button>
                     </div>
                     </Route>
