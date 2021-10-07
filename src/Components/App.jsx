282677b2ee3e12aabbfba2c2cc4aa513843227e3
AnatolijusZ <zubkovas.a@gmail.com>
2021-10-07 12:21:50 +0300

    code changes

--- src/Components/App.jsx
+++ src/Components/App.jsx
@@ -1,84 +1,83 @@
-import React, {useState} from "react";
-import { BrowserRouter as Router, Switch, Route, Link} from 'react-router-dom';
+import React, { useState } from "react";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 import CreateChar from "./CreateChar";
 import getId from "../Shared/getId";
 
-
 function App() {
-    
-    const [characters, setCharacters] = useState([]);
-    const [nameInput, setNameInput] = useState("");
-    const [str, setStr] = useState(5);
-    const [vit, setVit] = useState(5);
-    const [dex, setDex] = useState(5);
+  const [characters, setCharacters] = useState([]);
+  const [nameInput, setNameInput] = useState("");
+  const [str, setStr] = useState(5);
+  const [vit, setVit] = useState(5);
+  const [dex, setDex] = useState(5);
 
-    
+  const newCharacter = () => {
+    const player = {
+      id: getId(),
+      name: nameInput,
+      strength: str,
+      vitality: vit,
+      dexterity: dex,
+    };
+    const charactersCopy = characters.slice();
+    charactersCopy.push(player);
+    setCharacters(charactersCopy);
+    localStorage.setItem("allCharacters", JSON.stringify(charactersCopy));
+  };
 
-    const newCharacter = () => {
-       const player = {
-           id: getId(),
-           name: nameInput,
-           strength: str,
-           vitality: vit,
-           dexterity: dex
-       }
-       const charactersCopy = characters.slice();
-       charactersCopy.push(player);
-       setCharacters (charactersCopy);
-       localStorage.setItem('allCharacters', JSON.stringify(charactersCopy));
+  const editName = (id, name) => {
+    const charactersCopy = characters.slice();
+    for (let i = 0; i < charactersCopy.length; i++) {
+      if (charactersCopy[i].id == id) {
+        charactersCopy[i].name = nameInput;
+        break;
+      }
     }
+    setCharacters(charactersCopy);
+    localStorage.setItem("allCharacters", JSON.stringify(charactersCopy));
+  };
 
-    
-    const editName =(id, name) => {
-        const charactersCopy = characters.slice();
-        for(let i=0; i<charactersCopy.length; i++) {
-            if(charactersCopy[i].id == id){
-                charactersCopy[i].name = nameInput;
-                break;
-            }
-        }
-        setCharacters (charactersCopy);
-        localStorage.setItem('allCharacters', JSON.stringify(charactersCopy));
+  const strIncrease = (id, strength) => {
+    const charactersCopy = characters.slice();
+    for (let i = 0; i < charactersCopy.length; i++) {
+      if (charactersCopy[i].id == id) {
+        charactersCopy[i].strength++;
+        break;
+      }
     }
+    setCharacters(charactersCopy);
+    localStorage.setItem("allCharacters", JSON.stringify(charactersCopy));
+  };
 
-        const strIncrease = (id, strength) => {
-            const charactersCopy = characters.slice();
-                for(let i=0; i<charactersCopy.length; i++) {
-                    if (charactersCopy[i].id == id) {
-                        charactersCopy[i].strength++;
-                        break;
-                    }
-                    
-                }
-                setCharacters (charactersCopy);
-                localStorage.setItem('allCharacters', JSON.stringify(charactersCopy));
-        }
+  return (
+    <Router>
+      <Switch>
+        <Route exact path={"/"}>
+          <div className="main-menu">
+            <Link to="/create">
+              <button onClick={() => newCharacter()}>Create character</button>
+            </Link>
+            <button>Load existing character (not implemented)</button>
+          </div>
+        </Route>
 
-    
-     
-        return (
-            <Router>
-                <Switch>            
-                    <Route exact path={"/"}>
-                    <div className = "main-menu">
-                        <Link to="/create"> 
-                        <button  onClick={() => newCharacter()}>Create character</button>
-                        {characters.map((character) => <CreateChar key={character.id} id={character.id} str={character.strength} vit={character.vitality} dex={character.dexterity} setName={editName} strInc={strIncrease}/>)} 
-                        </Link>
-                        <button>Load existing character (not implemented)</button>
-                    </div>
-                    </Route>
-
-                    <Route path={"/create"}>
-                        <div className="char-creation-menu">
-                        <CreateChar/>
-                        </div>
-                    </Route>
-                </Switch>
-            </Router>
-        )
-    
-    
+        <Route path={"/create"}>
+          <div className="char-creation-menu">
+            {characters.map((character) => (
+              <CreateChar
+                key={character.id}
+                id={character.id}
+                str={character.strength}
+                vit={character.vitality}
+                dex={character.dexterity}
+                setName={editName}
+                strInc={strIncrease}
+              />
+            ))}
+          </div>
+        </Route>
+      </Switch>
+    </Router>
+  );
 }
 
 export default App;
--- src/Components/CreateChar.jsx
+++ src/Components/CreateChar.jsx
@@ -1,39 +1,33 @@
-import React, {useState} from "react";
-import getId from '../Shared/getId';
-import { BrowserRouter as Router, Switch, Route, Link} from 'react-router-dom';
+import React, { useState } from "react";
+//import getId from '../Shared/getId';
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 
 function CreateChar(id, name, str, dex, vit, setName, strInc) {
- 
-    const [nameInput, setNameInput] = useState("");
+  const [nameInput, setNameInput] = useState("");
 
+  const NameInputHandler = (e) => {
+    setNameInput(e.target.value);
+  };
 
-
-    const NameInputHandler= (e) => {
-        setNameInput(e.target.value)
-        };
-
-    
-        return (<>
-           
-            <div className="stats">
-            Strength: {str}
-            <button onClick={() => strInc()}>Increase</button>
-            Vitality: {vit}
-            <button>Increase</button>
-            Dexterity: {dex}
-            <button>Increase</button>
-
-            Enter your character name:
-            <input type="text" value={nameInput} onChange={NameInputHandler} />
-            <button onClick={()=> setName(nameInput)}>Submit</button>                      
-                    <Link to="/main">
-                        <button >Start adventure</button>
-                    </Link>                    
-        </div>
-        {console.log(nameInput)}
-        {console.log(str)}
-    
-        </>
-    )
+  return (
+    <>
+      <div className="stats">
+        Strength: {str}
+        <button onClick={() => strInc()}>Increase</button>
+        Vitality: {vit}
+        <button>Increase</button>
+        Dexterity: {dex}
+        <button>Increase</button>
+        Enter your character name:
+        <input type="text" value={nameInput} onChange={NameInputHandler} />
+        <button onClick={() => setName(id, nameInput)}>Submit</button>
+        <Link to="/main">
+          <button>Start adventure</button>
+        </Link>
+      </div>
+      {console.log(nameInput)}
+      {console.log(str)}
+    </>
+  );
 }
 export default CreateChar;
